cba5cc3ed0d2d2defbe2355330513f4201f1d1de
Alec Martin <alec@composable.finance>
2022-04-12 17:20:44 -0700

    digit builder

--- src/digit.rs
+++ src/digit.rs
@@ -1,110 +1,56 @@
-// parameters to set. should be a macro? const fn?
+use paste::paste;
 
-/* 8 bit
+// This is better but it can still be improved. It would be really nice to only have to call build_digit!(2) to get u16 digits.
 
-pub type Digit = u8;
+macro_rules! build_digit {
+    ($bits: expr, $double_bits: expr) => {
+        paste!{
+            pub const DIGITBITS: usize = $bits;
+            pub const DIGITBYTES: usize = DIGITBITS / 8;
+            pub type Digit = [<u $bits>];
+            pub type SignedDigit = [<i $bits>];
+            
+            pub const DOUBLEBITS: usize = $double_bits;
+            pub const DOUBLEBYTES: usize = 2*DIGITBYTES;
+            pub type DoubleDigit = [<u $double_bits>];
+            pub type SignedDoubleDigit = [<i $double_bits>];
 
-pub const DIGITBYTES: usize = 1;
+            pub const ALLONES: Digit = (-1 as SignedDigit) as Digit;
+            pub const GREATESTBIT: Digit = 1 << (DIGITBITS - 1);
 
-pub type SignedDigit = i8;
-
-pub type Double = u16;
-
-pub type SignedDouble = i16;
-
-pub fn digit_from_bytes(bytes: &[u8]) -> Digit {
-    Digit::from_le_bytes([bytes[0]])
-}
-
-// */
-
-//* 16 bit
-
-pub type Digit = u16;
-
-pub const DIGITBYTES: usize = 2;
-
-pub type SignedDigit = i16;
-
-pub type Double = u32;
-
-pub type SignedDouble = i32;
-
-pub fn digit_from_bytes(bytes: &[u8]) -> Digit {
-    Digit::from_le_bytes([bytes[0], bytes[1]])
-}
-
-// */
-
-/* 32 bit
-
-pub type Digit = u32;
-
-pub const DIGITBYTES: usize = 4;
-
-pub type SignedDigit = i32;
-
-pub type Double = u64;
-
-pub type SignedDouble = i64;
-
-pub fn digit_from_bytes(bytes: &[u8]) -> Digit {
-    Digit::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3]])
-}
-
-// */
-
-/* 64 bit
-
-pub type Digit = u64;
-
-pub const DIGITBYTES: usize = 8;
-
-pub type SignedDigit = i64;
-
-pub type Double = u128;
-
-pub type SignedDouble = i128;
-
-pub fn digit_from_bytes(bytes: &[u8]) -> Digit {
-    Digit::from_le_bytes([bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7]])
-}
-// */
-
-// done with parameters
-
-pub const DIGITBITS: usize = 8 * DIGITBYTES;
-
-pub const ALLONES: Digit = (-1 as SignedDigit) as Digit;
-
-pub const DOWNCASTMASK: Double = ALLONES as Double;
-
-pub const GREATESTBIT: Digit = 1 << (DIGITBITS - 1);
-
-macro_rules! add {
-    ($a: expr, $b: expr, $result: expr, $carry: expr) => {
-        let res = ($a as Double) + ($b as Double);
-        $result = res as Digit;
-        $carry = (res >> DIGITBITS) as Digit;
-    };
-    ($a: expr, $b: expr, $result: expr) => {
-        let res = ($a as Double) + ($b as Double);
-        $result = res as Digit;
+            pub fn digit_from_bytes(bytes: &[u8]) -> Digit {
+                Digit::from_le_bytes(bytes.try_into().unwrap())
+            }
+            
+            macro_rules! add {
+                ($a: expr, $b: expr, $result: expr, $carry: expr) => {
+                    let res = ($a as DoubleDigit) + ($b as DoubleDigit);
+                    $result = res as Digit;
+                    $carry = (res >> DIGITBITS) as Digit;
+                };
+                ($a: expr, $b: expr, $result: expr) => {
+                    let res = ($a as Double) + ($b as Double);
+                    $result = res as Digit;
+                };
+            }
+            
+            pub(crate) use add;
+            
+            macro_rules! mul {
+                ($a: expr, $b: expr, $result: expr, $carry: expr) => {
+                    let res = ($a as DoubleDigit) * ($b as DoubleDigit);
+                    $result = res as Digit;
+                    $carry = (res >> DIGITBITS) as Digit;
+                };
+                ($a: expr, $b: expr, $result: expr) => {
+                    let res = ($a as Double) * ($b as Double);
+                    $result = res as Digit;
+                };
+            }
+            
+            pub(crate) use mul;
+        }
     };
 }
 
-pub(crate) use add;
-
-macro_rules! mul {
-    ($a: expr, $b: expr, $result: expr, $carry: expr) => {
-        let res = ($a as Double) * ($b as Double);
-        $result = res as Digit;
-        $carry = (res >> DIGITBITS) as Digit;
-    };
-    ($a: expr, $b: expr, $result: expr) => {
-        let res = ($a as Double) * ($b as Double);
-        $result = res as Digit;
-    };
-}
-
-pub(crate) use mul;
+build_digit!(16, 32);
