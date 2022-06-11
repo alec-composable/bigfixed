d49c7f1ceab57dfecc580632f02a87ab0c5e9d29
Alec Martin <alec@composable.finance>
2022-06-11 12:10:12 -0700

    div

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use bigfixed::*;
-//use paste::paste;
-
-pub fn main() {
-    let scheme = schemes::SCHEME_F64;
-    let n = big_fixed_from!(scheme, 1);
-    let d = BigFixed::from(9);
-    println!("{:}", n / d);
-}
